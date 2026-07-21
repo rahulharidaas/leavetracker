8b979b222c438e068e1c60ca6b97e04fd0d15e83
rahul-haridaas <rahul@sleeksky.com>
2026-07-21 23:48:34 +0530

    Add password Reset Feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
 from .import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns =[
     path('login/',views.login_view, name='login'),
@@ -13,6 +14,16 @@ urlpatterns =[
     path('admin/employees/', views.admin_employees, name='admin_employees'),
     path('admin/department/', views.admin_department, name='admin_department'),
     path('admin/settings/', views.admin_settings, name='admin_settings'),
+    
+
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        html_email_template_name='registration/password_reset_email.html',
+        email_template_name='registration/password_reset_email.txt',
+    ), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 
 
 
